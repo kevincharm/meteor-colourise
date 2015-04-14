af0c536573e155a90617af88b1b7577c95258283
Kevin Tjiam <kevin@kevincharm.com>
2015-04-14 23:02:51 +0800

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "hellstad:colourise",
 	summary: "Design quicker. Click to change any element's colours!",
-	version: "0.1.1",
+	version: "0.1.3",
 	git: "https://github.com/hellstad/meteor-colourise",
 	debugOnly: true
 });
