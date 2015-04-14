40cbba5c374e53a5e93a01ee3604c9dd793453eb
hellstad <kevin@kevincharm.com>
2015-04-14 23:11:55 +0800

    add server files

---
+++ server/colourise.js
@@ -0,0 +1,38 @@
+ColouriseElements = new Mongo.Collection('colouriseelements');
+ColourisePalette = new Mongo.Collection('colourisepalette');
+
+Meteor.publish('colouriseelements', function () {
+	return ColouriseElements.find({});
+});
+
+Meteor.publish('colourisepalette', function () {
+	return ColourisePalette.find({});
+});
+
+Meteor.methods({
+	insertCE: function (target, targetFgColour, targetBgColour) {
+		ColouriseElements.insert({
+			nodeSelector: target.selector,
+			nodeEnabled: true,
+			fgColour: targetFgColour ? targetFgColour : '',
+			bgColour: targetBgColour ? targetBgColour : ''
+		});
+	},
+	updateCE: function (target, targetFgColour, targetBgColour) {
+		ColouriseElements.update({nodeSelector: target.selector}, {$set: {
+			nodeEnabled: true,
+			fgColour: targetFgColour ? targetFgColour : '',
+			bgColour: targetBgColour ? targetBgColour : ''
+		}}, {upsert: true});
+	},
+	resetCE: function () {
+		ColouriseElements.remove({});
+		return true;
+	},
+	insertColour: function (newColour) {
+		ColourisePalette.update({colour: newColour}, {$set: {colour: newColour}}, {upsert: true});
+	},
+	resetColours: function (newColour) {
+		ColourisePalette.remove({});
+	}
+});
