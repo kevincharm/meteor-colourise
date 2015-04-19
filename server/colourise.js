3688a44ded8f15d65638a566f7d1ea026d528668
Kevin Tjiam <kevin@kevincharm.com>
2015-04-20 00:00:18 +0800

    arrays

--- server/colourise.js
+++ server/colourise.js
@@ -11,15 +11,12 @@ Meteor.publish('colourisepalette', function () {
 
 Meteor.methods({
 	insertCE: function (target, targetFgColour, targetBgColour) {
-		// make this take an array
-		//_.each(targetFgColour, function (colour) {
-			ColouriseElements.insert({
-				nodeSelector: target.selector,
-				nodeEnabled: true,
-				fgColour: targetFgColour ? targetFgColour : '',
-				bgColour: targetBgColour ? targetBgColour : ''
-			});
-		//});
+		ColouriseElements.insert({
+			nodeSelector: target.selector,
+			nodeEnabled: true,
+			fgColour: targetFgColour ? targetFgColour : '',
+			bgColour: targetBgColour ? targetBgColour : ''
+		});
 	},
 	updateCE: function (target, targetFgColour, targetBgColour) {
 		ColouriseElements.update({nodeSelector: target.selector}, {$set: {
@@ -33,7 +30,13 @@ Meteor.methods({
 		return true;
 	},
 	insertColour: function (newColour) {
-		ColourisePalette.update({colour: newColour}, {$set: {colour: newColour}}, {upsert: true});
+		if (typeof newColour == 'array') {
+			_.each(newColour, function (cc) {
+				ColourisePalette.update({colour: cc}, {$set: {colour: cc}}, {upsert: true});
+			});
+		} else {
+			ColourisePalette.update({colour: newColour}, {$set: {colour: newColour}}, {upsert: true});
+		}
 	},
 	resetColours: function (newColour) {
 		ColourisePalette.remove({});
